0b2083c89102901f3e0c666891f15bec80e86b69
timothy <timothy.waweru1@student.moringaschool.com>
2025-03-12 10:16:03 +0300

    math

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+let multiply = 31 * 2
+console.log(multiply);
+
+const random = 7
+Math.floor (Math.random() * 10) + 1
+
+
+
+const num3 = 34
+const num4 = 10
+let mod = 34 % 10
+console.log(mod);
+
+
+const max = 20
+Math.max (1,2,7,9,13,15,19,20)
+
+
+
+
